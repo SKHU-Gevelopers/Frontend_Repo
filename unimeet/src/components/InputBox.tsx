fe3810e125db50ca7cac22fc2f5b0b4ac36dd8c5
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2023-05-31 00:33:12 +0900

    feat: input 컴포넌트 분리
    컴포넌트 분리 및 props 전달 기능 + style

---
+++ unimeet/src/components/InputBox.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+interface InputBoxProps {
+  value: string;
+  onChange: (value: string) => void;
+  defaultValue: string;
+}
+
+const InputBox: React.FC<InputBoxProps> = ({
+  value,
+  onChange,
+  defaultValue,
+}) => {
+  const [inputValue, setInputValue] = useState(defaultValue);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <>
+      <InputDiv
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        required
+      />
+    </>
+  );
+};
+
+const InputDiv = styled.input`
+  font-family: monospace;
+  max-width: 190px;
+  outline: none;
+  border: none;
+  border-bottom: 2px solid #674ff4;
+  padding: 5px;
+  background-color: #faebd700;
+  transition: 0.3s;
+  color: #5b5b5b;
+  &:focus {
+    box-shadow: 0 2px 4px #312576;
+    background-color: #c0b5ff42;
+    transform: translateY(-1px);
+    transition: all 1s;
+  }
+`;
+
+export default InputBox;
