d6015909c173d5fb5ad1f0d9fbe6876c10d675d8
baeksehee <tpgmlqor@gmail.com>
2023-08-21 16:41:14 +0900

    styled: meetingLogs 페이지 SwitchDiv CSS 추가

--- unimeet/src/pages/meetingLogs.tsx
+++ unimeet/src/pages/meetingLogs.tsx
@@ -4,11 +4,87 @@ import styled from "styled-components";
 export default function MeetingLogs() {
   return (
     <>
-      <SentRequests />
-      <ReceivedRequests />
+      <SwitchDiv>
+        <ReceivedRequestsBtn>받은 신청함</ReceivedRequestsBtn>
+        <SentRequestsBtn>보낸 신청함</SentRequestsBtn>
+      </SwitchDiv>
+      <MainBox>
+        <Article></Article>
+      </MainBox>
     </>
   );
 }
+
+const SwitchDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  width: 100%;
+  height: 5vh;
+
+  gap: 2%;
+
+  margin-top: 8vh;
+  padding-left: 2%;
+  padding-right: 2%;
+`;
+
+const ReceivedRequestsBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 50%;
+  height: 5vh;
+
+  border-radius: 1.1rem 1.1rem 0 0;
+
+  background-color: #674ff4;
+
+  color: white;
+
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
+const SentRequestsBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 50%;
+  height: 5vh;
+
+  border-radius: 1.1rem 1.1rem 0 0;
+
+  background-color: #674ff4;
+
+  color: white;
+
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
+const MainBox = styled.div`
+  display: flex;
+  align-content: center;
+
+  width: 100%;
+  height: auto;
+
+  background-color: #efe3ff;
+  opacity: 0.97;
+`;
+
+const Article = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  min-height: 78vh;
+  // 0.몇으로 하면 자동적으로 길이가 길어짐에 따라(100vh를 넘었을 때) 색의 경계선이 보임
+`;
 
 // 보낸 신청함
 function SentRequests() {
